82921c70c979fe7052c2c8ad38995accff91cb2e
Tri Do <trido1903@gmail.com>
2020-01-25 00:13:05 -0800

    added generate palette colors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
 
 function App() {
+  generatePalette(seedColors[4])
   return (
     <div>
-      <Palette colors={seedColors[4].colors}/>
+      <Palette colors={seedColors[4].colors} />
     </div>
   );
 }
