4a2ea82e15135edeef10c83874923b7a16797881
Tri Do <trido1903@gmail.com>
2020-02-09 22:18:39 -0800

    added drawer to newpaletteform

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { generatePalette } from "./colorHelpers";
 import { Switch, Route } from "react-router-dom";
 import PaletteList from "./PaletteList";
 import SingleColorPalette from "./SingleColorPalette";
-import PaletteForm from "./PaletteForm";
+import NewPaletteForm from "./NewPaletteForm";
 
 function findPalette(id) {
   return seedColors.find(palette => {
@@ -17,7 +17,7 @@ function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/palette/new" render={() => <PaletteForm />} />
+        <Route exact path="/palette/new" render={() => <NewPaletteForm />} />
         <Route
           exact
           path="/"
